eb09e6ec00c5e093f5738d53845948f0eadf309e
cipayne36 <cpayne36@gatech.edu>
2018-12-05 22:04:48 -0500

    Initial Test Commit

--- Adventures/Program.cs
+++ Adventures/Program.cs
@@ -10,6 +10,13 @@ namespace Adventures
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Oh! Hello there! My name is Zigzo. What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} is it? That's a weird name... You're not from around here, are you? How old are you anyway?", name);
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You're {0}, huh?", age);
+            
+            Console.ReadLine();
         }
     }
 }
