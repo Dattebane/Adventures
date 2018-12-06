2409f8746b7d8256d5074dbe3b4409ade0266c42
cipayne36 <cpayne36@gatech.edu>
2018-12-05 23:25:51 -0500

    Playing around with classes and stuff

--- Adventures/Program.cs
+++ Adventures/Program.cs
@@ -10,13 +10,66 @@ namespace Adventures
     {
         static void Main(string[] args)
         {
+            // Just getting my bearings here...
+            drawValley(36);
+            Shinobi player = new Shinobi();
             Console.WriteLine("Oh! Hello there! My name is Zigzo. What's your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("{0} is it? That's a weird name... You're not from around here, are you? How old are you anyway?", name);
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You're {0}, huh?", age);
-            
+            player.setName(Console.ReadLine());
+            Console.WriteLine("{0} is it? That's a weird name... You're not from around here, are you? How old are you anyway?", player.getName());
+            player.setAge(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("You're {0}, huh?", player.getAge());
             Console.ReadLine();
+           
+
+        }
+        static void drawValley(int n)
+        {
+            for(int i = 0; i <= n; i++) //each row
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int k = 0; k < ((2 * n) - (2 * i)); k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        class Shinobi
+        {
+
+            // properties
+            private string name;
+            private int age;
+            public Shinobi()
+            {
+                Console.WriteLine("A new shinobi has entered the battlefield");
+            }
+            // getters
+            public string getName()
+            {
+                return name;
+            }
+            public int getAge()
+            {
+                return age;
+            }
+
+            // setters
+            public void setName(string name)
+            {
+                this.name = name;
+            }
+            public void setAge(int age)
+            {
+                this.age = age;
+            }
         }
     }
 }
